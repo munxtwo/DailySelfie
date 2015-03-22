caf608ea195f20eca1f715e7c98c347c05608671
ktay <munxtwo@gmail.com>
2015-03-21 23:48:13 -0700

    Moved methods from CameraUtil into MainActivity.

--- src/com/ktay/DailySelfie/MainActivity.java
+++ src/com/ktay/DailySelfie/MainActivity.java
@@ -3,20 +3,29 @@ package com.ktay.DailySelfie;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.GridView;
 import android.widget.Toast;
-import com.ktay.DailySelfie.util.CameraUtil;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "DailySelfie";
     private static final int CAPTURE_IMAGE_ACTIVITY_REQ_CODE = 100;
+    private static final int MEDIA_TYPE_IMAGE = 1;
+    private static final String DIR_NAME = "DailySelfie";
+
+    private File mediaStorageDir;
 
     /**
      * Called when the activity is first created.
@@ -26,8 +35,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        initMediaStorageDir();
+
         GridView gridView = (GridView) findViewById(R.id.gridView);
-        gridView.setAdapter(new ImageAdapter());
+        gridView.setAdapter(new ImageAdapter(this));
     }
 
     @Override
@@ -66,13 +77,56 @@ public class MainActivity extends Activity {
         Intent captureImageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         if (captureImageIntent.resolveActivity(getPackageManager()) != null) {
-            File imageFile = CameraUtil.getOutputMediaFile(CameraUtil.MEDIA_TYPE_IMAGE);
+            File imageFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
 
             if (imageFile == null) {
-                Toast.makeText(this, R.string.error_unable_to_access_storage, Toast.LENGTH_SHORT);
+                Toast.makeText(this, R.string.error_unable_to_access_storage, Toast.LENGTH_SHORT).show();
             } else {
-                captureImageIntent.putExtra(MediaStore.EXTRA_OUTPUT, CameraUtil.getOutputMediaFileUri(imageFile));
+                captureImageIntent.putExtra(MediaStore.EXTRA_OUTPUT, getOutputMediaFileUri(imageFile));
                 startActivityForResult(captureImageIntent, CAPTURE_IMAGE_ACTIVITY_REQ_CODE);
+            }
+        }
+    }
+
+    private File getOutputMediaFile(int type){
+        // Create the storage directory if it does not exist
+        if (mediaStorageDir == null){
+            return null;
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    private Uri getOutputMediaFileUri(File file) {
+        return Uri.fromFile(file);
+    }
+
+    private void initMediaStorageDir() {
+        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Log.e(TAG, "media storage is not mounted");
+            Toast.makeText(this, R.string.error_media_storage_not_mounted, Toast.LENGTH_SHORT).show();
+            mediaStorageDir = null;
+        }
+
+        mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), DIR_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                Log.e(TAG, "failed to create directory");
+                Toast.makeText(this, R.string.error_failed_to_create_dir, Toast.LENGTH_SHORT).show();
+
+                mediaStorageDir = null;
             }
         }
     }
